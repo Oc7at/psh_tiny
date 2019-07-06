c330afdb00ee303d4b7ecafdb08e41c32bf35c0b
Lucas Stil <lucas@platform.sh>
2019-07-06 23:43:24 +0200

    Added a failsafe on matches.is_present("relation")

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use clap::ArgMatches;
 pub fn run_relationships<'a>(matches: &ArgMatches<'a>) {
     let relationships = psh_config::get_json_from_var("PLATFORM_RELATIONSHIPS").unwrap();
     if matches.is_present("relation") {
+        if !psh_config::has_relationship(matches.value_of("relation").unwrap()) {
+            println!("This relationship does not exist.");
+            std::process::exit(1);
+        }
         if matches.is_present("element") {
             printer::print_relationship_elem(
                 &relationships,
@@ -24,8 +28,8 @@ fn main() {
         println!("This is not a valid Platform.sh environment.");
         std::process::exit(1)
     }
-    let matches = args::arg_parser();
-    if let Some(matches) = matches.subcommand_matches("relationships") {
+    let commands = args::arg_parser();
+    if let Some(matches) = commands.subcommand_matches("relationships") {
         run_relationships(matches);
     }
 }
